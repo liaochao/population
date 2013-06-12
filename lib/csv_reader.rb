7a67e0374cf8fb66246fc1cccecc200ba5b17d26
Chao Liao <chao.liao1989@gmail.com>
2013-06-12 19:38:08 -0400

    CSVReader

---
+++ lib/csv_reader.rb
@@ -0,0 +1,35 @@
+class CSVReader
+  attr_accessor :fname, :headers
+
+  def initialize(filename)
+    @fname = filename
+  end
+
+  def headers=(header_str)
+    @headers = header_str.split(',')
+    @headers.map! do |h|
+      new_header = h.gsub('"','')
+      new_header.strip!
+      new_header.underscore.to_sym
+    end
+  end
+
+  def create_hash (ary)
+    h = Array.new
+    @headers.each_with_index do |header, i|
+      value = ary[i].strip.gsub('"','')
+      h[header]=value unless value.empty?
+    end
+    h
+  end
+end
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
